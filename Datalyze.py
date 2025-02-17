f7bbfd96c16bf182baa71579435efc81db0d26c4
Beatriz Cardoso Cunha <72693632+beatrizcardc@users.noreply.github.com>
2025-02-17 09:32:54 -0300

    Update Datalyze.py

--- Datalyze.py
+++ Datalyze.py
@@ -97,8 +97,8 @@ if df is not None:
         df, modelo = prever_vendas(df)
         if df is not None:
             st.write("### 📈 Previsão de Vendas")
-                    st.write(f"### 📈 Previsão de Vendas em função de {variavel_grafico.capitalize()}")
-                        if variavel_grafico == 'dia_semana':
+            st.write(f"### 📈 Previsão de Vendas em função de {variavel_grafico.capitalize()}")
+                if variavel_grafico == 'dia_semana':
             dias_semana = {1: 'Segunda', 2: 'Terça', 3: 'Quarta', 4: 'Quinta', 5: 'Sexta', 6: 'Sábado', 7: 'Domingo'}
             df['dia_semana'] = df['dia_semana'].map(dias_semana)
         df_plot = df[[variavel_grafico, 'vendas', 'previsao_vendas']].groupby(variavel_grafico).mean()
